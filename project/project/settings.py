45beadb2e379698fd7ea453a7c5a876cd02c0970
vidhyarthi-uikey <vidhyarthiu2111@gmail.com>
2025-09-16 15:22:57 +0530

    update

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+# this is required for collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#optianal but recommended
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static'),]
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
